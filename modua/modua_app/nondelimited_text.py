d03dbedeaccfa56f2d82513f3924445746296957
Bobby Eshleman <eshleman@pdx.edu>
2016-05-10 15:35:45 -0700

    Improved docstrings for NonDelimitedText

--- modua/modua_app/nondelimited_text.py
+++ modua/modua_app/nondelimited_text.py
@@ -1,39 +1,46 @@
+#nondelimited_text.py
+
 import django
-'''
-
-    Request Structure:
-    string: "word"
-    langage: "langage"
-    
-    
-    Example Structure:
-    string: "天下無雙"
-    locale: "zh_HANT"
-
-
-    Actual string passed to view after urls.py:
-    "?string=天下無雙&locale=zhHANT"
-
-'''
 
 class NonDelimitedText:
+    '''A class for convenient processing of texts with no delimiter.
+
+    A special property called sub_units allows easy iteration through the query
+    choices defined by the MODUA algorithm for querying non-'spaced delimited'
+    strings.
+
+
+    '''
 
     def __init__(self, string, locale=None):
+        '''Constructor that sets the string and locale (language) of the
+        string
+        '''
         self.string = string
         self.locale = locale
 
     @property
     def sub_units(self):
-        ''' 'ABCD' --> A AB ABC ABCD '''
+        '''Yields a generator of this object's string in segmented form.
+
+        Example:
+        If the object's string is 'ABCD', the generator will yield as follows:
+            'ABCD' --> A AB ABC ABCD 
+
+        '''
         lexeme = ''
         for c in self.string:
                 lexeme += c
                 yield lexeme
 
     def __str__(self):
+        '''Returns the string only, not the locale'''
         return self.string
 
     def __eq__(self, compare):
+        '''Returns true if the string and locale are equal to compare's string and 
+        locale
+        '''
         return (self.string == compare.string and 
                 self.locale == compare.locale)
     
