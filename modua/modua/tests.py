c291857e05cbdb669e41472504aa45584dd63e00
bobbyesh <eshleman@pdx.edu>
2016-05-20 11:48:20 -0700

    Gave NonDelimitedText its own tests.

--- modua/modua/tests.py
+++ modua/modua/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase, Client, RequestFactory
 from django.http import JsonResponse
 from django.db import models
-from modua_app.nondelimited_text import NonDelimitedText
 
 
 class TestRequests(TestCase):
@@ -22,7 +21,10 @@ class TestRequests(TestCase):
         response = self.client.get("/lookup/foo")
         self.assertEqual(response.status_code, 200, "/lookup/foo resturns status code 200)")
 
-    
+    def test_response_is_JsonResponse(self):
+        response = self.client.get(self.build_valid_url())
+        self.assertIsInstance(response, JsonResponse)
+
     def get_basic_request(self):
         '''Return a basic request with value pairs: string = test_string and
         locale = test_locale
@@ -30,25 +32,11 @@ class TestRequests(TestCase):
         url = self.build_valid_url()
         request = self.factory.get(url)
         return request;
-    
-    
+
     def build_valid_url(self):
         '''Returns a string that is a valid GET request and url'''
         string_pair = "string=" + self.test_string
         locale_pair =  "locale=" + self.test_locale
         url = "/lookup/?" + string_pair + "&" + locale_pair
         return url
-        
 
-
-
-    def test_nondelimited_text_generator(self):
-        '''Passes if a list built from the NonDelimitedText's sub_unit property is
-        equal to the list it should be equal too, the test_list
-        '''
-        text = NonDelimitedText(self.test_string, self.test_locale)
-        self.assertEqual(list(text.sub_units), self.test_list)
-
-    def test_response_is_JsonResponse(self):
-        response = self.client.get(self.build_valid_url())
-        self.assertIsInstance(response, JsonResponse)
