b414bd6c1d68d8a4933cfb41714eeba27623f450
bobbyesh <eshleman@pdx.edu>
2016-05-22 21:14:15 -0700

    Updated urls to accomodate /api/search/search_term URI structure, and renamed the "lookup" view to "search"

--- modua/modua/urls.py
+++ modua/modua/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^lookup/', include('modua_app.urls')),
+    url(r'^api/', include('modua_app.urls')),
 ]
