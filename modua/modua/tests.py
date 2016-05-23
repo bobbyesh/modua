72961b51ee6952f6f8e8d55e3c03a6ed522f1888
bobbyesh <eshleman@pdx.edu>
2016-05-22 21:12:46 -0700

    Refactored NonDelimitedText into a utility function and updated associated tests.

--- modua/modua/tests.py
+++ modua/modua/tests.py
@@ -1,13 +1,11 @@
 from django.test import TestCase, Client, RequestFactory
-from django.db import models
-from django.http import JsonResponse
-import mock
+from rest_framework.response import Response
 
 
 class TestRequests(TestCase):
 
-    test_string = "天下無雙"
-    test_locale = "zh_HANT"
+    search_string = "天下無雙"
+    search_locale = "zh_HANT"
     test_list = ["天","天下","天下無","天下無雙"]
     test_definition = "There are no two things in the world that are the same"
 
@@ -16,21 +14,9 @@ class TestRequests(TestCase):
         self.client = Client()
         self.factory = RequestFactory()
 
-    def test_request_successful(self):
-        '''Passes if a generic GET request to 'oursite.com/lookup/' returns a
+    def test_request_to_api_search(self):
+        '''Passes if a generic GET request to 'oursite.com/search/foo' returns a
         response with status code 200.
         '''
-        response = self.client.get("/lookup/foo")
-        self.assertEqual(response.status_code, 200, "/lookup/foo resturns status code 200)")
-
-    def test_response_is_JsonResponse(self):
-        response = self.client.get(self.build_valid_url())
-        self.assertIsInstance(response, JsonResponse)
-
-    def build_valid_url(self):
-        '''Returns a string that is a valid GET request and url'''
-        string_pair = "string=" + self.test_string
-        locale_pair =  "locale=" + self.test_locale
-        url = "/lookup/?" + string_pair + "&" + locale_pair
-        return url
-
+        response = self.client.get("/api/search/" + self.search_string)
+        self.assertEqual(response.status_code, 200, "api/search/" + self.search_string + " returns status code 200)")
--- modua/modua_app/views.py
+++ modua/modua_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
-def lookup(request):
-    return HttpResponse("good")
+def search(request):
+    return HttpResponse("good", status=200)
 
