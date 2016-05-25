f43daf28c96f8b734ea4a19f1dd55f0c3c4dbba1
bobbyesh <eshleman@pdx.edu>
2016-05-25 16:07:13 -0700

    Updated urls to require api version 0.1 (e.g., "/api/0.1/search/")

--- modua/modua/urls.py
+++ modua/modua/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include('modua_app.urls')),
+    url(r'^api/0.1/', include('modua_app.urls')),
 ]
