9c42f2d682987c8b0f55d345795aaf86cf47a9c5
bobbyesh <eshleman@pdx.edu>
2016-05-28 17:19:36 -0700

    Updated urls.py and views

--- modua/main_site/urls.py
+++ modua/main_site/urls.py
@@ -1,13 +1,34 @@
 from django.conf.urls import url, include
-from django.contrib import admin
 from . import views
 
 urlpatterns = [
 
-    url(r'^$', views.Home.as_view(), name="home"),
-    url(r'^$', views.Login.as_view(), name ="login"),
-    url(r'^$', views.UserPage.as_view(), name="user_page"),
-    url(r'^$', views.WordList.as_view(), name="word_list"),
-    url(r'^$', views.WordDetail(), name="word_detail"),
+    # TODO regex to match word
+    url(r'^$', views.HomeView.as_view(), name="home"),
+    url(r'^login/$', views.LoginView.as_view(), name="login"),
+
+    # TODO regex to match usernames, i.e., user/{username}
+    url(r'^user/(regex_for_username)$', views.UserPageView.as_view(), name="user_page"),
+
+    # The tail end of this needs to be a key-value pair indicating which
+    # language the word list is for
+    #
+    # There is no '$' at the end of this URL pattern, so any key-value pairs
+    # that follow will be passed to the view.
+    url(r'^/user/word-list/', views.WordListView.as_view(), name="word_list"),
+
+    # TODO regex to match username and word, i.e., user/{username}/word/{word}
+    # The tail end of this will have to have key value pairs indicating which
+    # word and which language.
+    url(r'^/user/(username_regex_)/word/', views.WordDetailView(), name="word_detail"),
+
+
+    # TODO regex to match username, i.e., user/{username}/add-definition
+    # There will need to have a key value pair indicating which word a definition
+    # is being added for.
+    #
+    # There is no '$' at the end of this URL pattern, so any key-value pairs
+    # that follow will be passed to the view.
+    url(r'^/user/(username_regex_)/word/add-definition/', views.AddDefinitionView(), name="add_definition"),
 
 ]
--- modua/modua_app/test_utils.py
+++ modua/modua_app/test_utils.py
@@ -7,9 +7,11 @@ class TestUtils(SimpleTestCase):
 
     test_string = "天下無雙"
     test_locale = "zh_HANT"
-    test_list = ["天","天下","天下無","天下無雙"]
+    test_list = ["天", "天下", "天下無", "天下無雙"]
 
     def test_build_generator(self):
-        '''Test that the generator from search_segments yields a generator that produces the correct order'''
+        '''Test that the generator from search_segments yields a generator
+        that produces the correct order
+        '''
         segments = utils.search_segments(self.test_string)
         self.assertEqual(list(segments), self.test_list)
