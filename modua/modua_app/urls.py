0611d2500e6240f65fb71be9874059df9c13f991
etokrug <etokrug@gmail.com>
2016-05-29 20:15:24 -0700

    Settings file changed for staticfiles handling.

--- modua/modua_app/urls.py
+++ modua/modua_app/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^/search/(?P<language_tag>[\w-]+)/(?P<word>[\w-]+)', views.Search.as_view(), name='search'),
+    url(r'^search/(?P<language_tag>[\w-]+)/(?P<word>[\w-]+)', views.Search.as_view(), name='search'),
 ]
