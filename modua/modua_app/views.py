103c79a5d887f766573ac6f2c2b5ab71bcf53058
bobbyesh <bobbyeshleman@gmail.com>
2016-05-29 19:37:54 -0700

    Built GET request search views and tests

--- modua/modua_app/views.py
+++ modua/modua_app/views.py
@@ -1,15 +1,45 @@
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
+
 from .models import Definitions
-from .serializers import DefinitionSerializer
+from .serializers import DefinitionsSerializer
+from .utils import segmentize, is_delimited
 
 
-class Search(APIView):
+class SearchView(APIView):
 
     renderer_classes = (JSONRenderer,)
 
-    def get(self, request, format=None):
-        definitions = Definitions.objects.all()
-        serializer = DefinitionSerializer(definitions, many=True)
+    def get(self, request, language, word, format=None):
+        if is_delimited(language):
+            try:
+                definition = Definitions.objects.get(word_character=word)
+                serializer = DefinitionsSerializer(definition, many=False)
+            except ObjectDoesNotExist:
+                return Response(status_code=404)
+        else:
+            definitions = list()
+            for seg in segmentize(word):
+                definitions = self.get_all_subsegment_definitions(word)
+            if not definitions:
+                return Response(status_code=404)
+            if len(definitions) == 1:
+                definition = definitions[0]
+                serializer = DefinitionsSerializer(definition, many=False)
+            else:
+                serializer = DefinitionsSerializer(definitions, many=True)
+
         return Response(serializer.data)
+
+    def get_all_subsegment_definitions(self, word):
+        definitions = list()
+        for seg in segmentize(word):
+            try:
+                result = Definitions.objects.get(word_character=seg)
+                definitions.append(result)
+            except ObjectDoesNotExist:
+                pass
+        return definitions
