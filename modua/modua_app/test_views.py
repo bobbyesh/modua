50f2282ce78e17d5dfa40f471d8b14a056f463a8
Bobby Eshleman <eshleman@pdx.edu>
2016-06-22 20:55:30 +0000

    Eliminated clunky member naming in test class.

--- modua/modua_app/test_views.py
+++ modua/modua_app/test_views.py
@@ -9,18 +9,10 @@ from .models import Definitions
 
 class TestViews(APITestCase):
 
-    text = "hello"
-    valid_lang = "en-US"
-    invalid_lang = "blah-lang"
-    url_one_word = "/api/0.1/search/en-US/hello"
-    url_search = "/api/0.1/search/en-US/"
-    url_nondelimited_word = "/api/0.1/search/zh-Hant/kool-aidxxxx"
-    nondelimited_url = "/api/0.1/search/zh-Hant/"
-    definition = "A greeting"
 
     def setUp(self):
-        Definitions.objects.create(word_character=self.text,
-                                           definition=self.definition)
+        Definitions.objects.create(word_character="hello",
+                                           definition="A greeting")
         Definitions.objects.create(word_character='kool-aid',
                                    definition='the koolest drink ever')
 
@@ -28,24 +20,24 @@ class TestViews(APITestCase):
         '''
         Passes if a valid search returns a status code of 200.
         '''
-        response = self.client.get(self.url_one_word, format='json')
+        response = self.client.get("/api/0.1/search/en-US/hello", format='json')
         self.assertEqual(response.status_code, 200)
 
     def test_term_search(self):
         '''
         Passes if the json returned from a valid request is the correct json.
         '''
-        response = self.client.get(self.url_one_word, format='json')
+        response = self.client.get("/api/0.1/search/en-US/hello", format='json')
         json = response.json()
-        self.assertEqual(json, {'word_character': self.text,
-                                    'definition': self.definition,
+        self.assertEqual(json, {'word_character': "hello",
+                                    'definition': 'A greeting',
                                     'transliteration': None })
     
     def test_bad_term_search(self):
         '''
         Passes if a request for a word not in the dictionary returns a status code 404.
         '''
-        response = self.client.get(self.url_search + "term_not_in_DB", format='json')
+        response = self.client.get("/api/0.1/search/en-US/term_not_in_DB", format='json')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
     
     def test_bad_url(self):
@@ -59,7 +51,7 @@ class TestViews(APITestCase):
         '''
         Passes if a the json returned from a valid search for a nondelimited word is correct.
         '''
-        response = self.client.get(self.url_nondelimited_word, format='json')
+        response = self.client.get("/api/0.1/search/zh-Hant/kool-aidxxxx", format='json')
         json = response.json()
         self.assertEqual(json, {'word_character': 'kool-aid',
                                     'definition': 'the koolest drink ever',
@@ -70,5 +62,5 @@ class TestViews(APITestCase):
         '''
         Passes if a request for a nondelimited word that isn't in the database returns a 404 response.
         '''
-        response = self.client.get(self.nondelimited_url + "term_not_in_DB", format='json')
+        response = self.client.get("/api/0.1/search/zh-Hant/term_not_in_DB", format='json')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
