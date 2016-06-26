da83693c3c3af6c8ead9606bcccf92cb670e2c1f
Bobby Eshleman <eshleman@pdx.edu>
2016-06-26 23:01:12 +0000

    Improved urls patterns.

--- modua/modua_app/urls.py
+++ modua/modua_app/urls.py
@@ -3,9 +3,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    url(r'^search/(?P<language>[\w-]+)/(?P<word>[\w-]+)', views.SearchView.as_view(), name='search'),
+    url(r'^languages/(?P<language>[\w-]+)/(?P<word>[\w-]+)', views.SearchView.as_view(), name='search'),
+    url(r'^languages/(?P<language>[\w-]+)/', views.LanguageWordlistView.as_view(), name='language_wordlist'),
     url(r'^languages/', views.LanguageListView.as_view(), name='language_list'),
-    url(r'^language/(?P<language>[\w-]+)/', views.LanguageWordlistView.as_view(), name='language_wordlist'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
