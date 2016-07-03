950ad798a44854427d2fb0f29af5032df66bcf75
Bobby Eshleman <eshleman@pdx.edu>
2016-07-03 22:27:10 +0000

    Removed redundant DefinitionsListSerializer.

--- modua/modua_app/serializers.py
+++ modua/modua_app/serializers.py
@@ -1,23 +1,17 @@
 from rest_framework import serializers
 from .models import Definitions, Languages
 
-
 class DefinitionsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Definitions
         fields = ('word_character', 'definition', 'transliteration')
 
 
-class DefinitionsListSerializer(serializers.ListSerializer):
-    child = DefinitionsSerializer
-    class Meta:
-        model = Definitions
-        fields = ('word_character', 'definition', 'transliteration')
 
 class LanguagesWordListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Languages
-        fields = ('language',)
+        fields = ('language', 'words')
     
 
 class LanguagesSerializer(serializers.ModelSerializer):
