40726129be33f2be1c56c302a1f4045de2d9b2e3
bobbyesh <eshleman@pdx.edu>
2016-07-08 17:34:08 -0700

    Basic Sphinx structure, again.

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:50,filenames:["index","modules/models"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to Mouse Over Dictionary with User Accounts&#8217;s documentation!","Models"],titleterms:{account:0,dictionari:0,document:0,indice:0,model:1,mous:0,over:0,tabl:0,user:0,welcom:0}})
