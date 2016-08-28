71317b29678c807306faa47f9ab3006eab6663d3
bobbyesh <eshleman@pdx.edu>
2016-08-28 11:47:14 -0700

    Factored out Editable and Authorable.

--- modua/api/models.py
+++ modua/api/models.py
@@ -1,33 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from core.behaviors import Timestampable
 from extras import CharNullField
 
 
-class Language(models.Model):
+class Language(Authorable, Editable, Timestampable, models.Model):
+    '''
+
+    .. TODO: Create fulltext index in DB
+
+    '''
     id = models.AutoField(null=False, primary_key=True, editable=False)
-    user_added = models.ForeignKey(User, related_name='user_added_lang', null=True)
-    user_updated = models.ForeignKey(User, related_name='user_updated_lang', null=True)
-    #TODO: Create fulltext index in DB
     language = models.CharField(null=True, max_length=150)
     script = models.CharField(null=True, max_length=300)
-    added = models.DateTimeField(null=True, editable=False)
-    updated = models.DateTimeField(null=True, editable=False)
-
-    def save(self, *args, **kwargs):
-        if not self.id:
-            self.added = timezone.now()
-        self.updated = timezone.now()
-        super(Language, self).save()
 
     def __str__(self):
         return '%s' % self.language
 
 
-class DictionaryAPI(models.Model):
+class DictionaryAPI(Authorable, Editable, Timestampable, models.Model):
     id = models.AutoField(null=False, primary_key=True, editable=False)
-    user_added = models.ForeignKey(User, related_name='user_added_dictionary_api', null=True)
-    user_updated = models.ForeignKey(User, related_name='user_updated_dictionary_api', null=True)
     # TODO: Create fulltext index in DB
     name = models.CharField(null=True, max_length=150)
     # This should be Usage, notes, and known issues
@@ -41,38 +34,20 @@ class DictionaryAPI(models.Model):
     api_key = models.CharField(null=True, max_length=500)
     # The key needed/issued to access the API - may not be necessary
     id_key = models.CharField(null=True, max_length=500)
-    added = models.DateTimeField(null=True, editable=False)
-    updated = models.DateTimeField(null=True, editable=False)
-
-    def save(self):
-        if not self.id:
-            self.added = timezone.now()
-        self.updated = timezone.now()
-        super(DictionaryAPI, self).save()
 
     def __str__(self):
         return self.api_name
 
 
-class WordType(models.Model):
+class WordType(Authorable, Editable, Timestampable, models.Model):
     id = models.AutoField(null=False, primary_key=True, editable=False)
-    user_added = models.ForeignKey(User, related_name='user_added_wordtype', null=True)
-    user_updated = models.ForeignKey(User, related_name='user_updated_wordtype', null=True)
     word_type = models.CharField(null=True, max_length=150)
-    added = models.DateTimeField(null=True, editable=False)
-    updated = models.DateTimeField(null=True, editable=False)
-
-    def save(self):
-        if not self.id:
-            self.added = timezone.now()
-        self.updated = timezone.now()
-        super(WordType, self).save()
 
     def __str__(self):
         return self.word_type
 
 
-class Definition(models.Model):
+class Definition(Timestampable, models.Model):
     '''
 
     ..TODO  Create fulltext index in DB
@@ -90,86 +65,39 @@ class Definition(models.Model):
     total_lookups = models.IntegerField(null=True)
     user_added = models.IntegerField(null=True)
     archived = models.BooleanField(default=False, null=False)
-    added = models.DateTimeField(null=True, editable=False)
-    updated = models.DateTimeField(null=True, editable=False)
-    archived_date = models.DateTimeField(null=True, editable=False)
-
-    def save(self, *args, **kwargs):
-        if not self.id:
-            self.added = timezone.now()
-        self.updated = timezone.now()
-        super(Definition, self).save()
 
     def __str__(self):
         return '%s' % self.word
 
 
-class Country(models.Model):
+class Country(Authorable, Editable, Timestampable, models.Model):
     id = models.AutoField(null=False, primary_key=True, editable=False)
-    user_added = models.ForeignKey(User, related_name='user_added_country', null=True)
-    user_updated = models.ForeignKey(User, related_name='user_updated_country', null=True)
     country_name = models.CharField(null=True, max_length=250)
-    added = models.DateTimeField(null=True, editable=False)
-    updated = models.DateTimeField(null=True, editable=False)
-
-    def save(self):
-        if not self.id:
-            self.added = timezone.now()
-        self.updated = timezone.now()
-        super(Country, self).save()
 
     def __str__(self):
         return self.country_name
 
 
-class Region(models.Model):
+class Region(Authorable, Editable, Timestampable, models.Model):
     id = models.AutoField(null=False, primary_key=True, editable=False)
     country_region = models.ForeignKey(Country, related_name='country_region', null=True)
-    user_added = models.ForeignKey(User, related_name='user_added_region', null=True)
-    user_updated = models.ForeignKey(User, related_name='user_updated_region', null=True)
     region = models.CharField(null=True, max_length=300)
-    added = models.DateTimeField(null=True, editable=False)
-    updated = models.DateTimeField(null=True, editable=False)
-
-    def save(self):
-        if not self.id:
-            self.added = timezone.now()
-        self.updated = timezone.now()
-        super(Region, self).save()
 
     def __str__(self):
         return self.region
 
 
-class City(models.Model):
+class City(Authorable, Editable, Timestampable, models.Model):
     id = models.AutoField(null=False, primary_key=True, editable=False)
     country_city = models.ForeignKey(Country, related_name='country_city', null=True)
     region_city = models.ForeignKey(Region, related_name='region_city', null=True)
-    user_added = models.ForeignKey(User, related_name='user_added_city', null=True)
-    user_updated = models.ForeignKey(User, related_name='user_updated_city', null=True)
     city_name = models.CharField(null=True, max_length=150)
-    added = models.DateTimeField(null=True, editable=False)
-    updated = models.DateTimeField(null=True, editable=False)
-
-    def save(self):
-        if not self.id:
-            self.added = timezone.now()
-        self.updated = timezone.now()
-        super(City, self).save()
 
     def __str__(self):
         return self.city_name
 
 
-class UserDefinition(models.Model):
+class UserDefinition(Timestampable, models.Model):
     id = models.AutoField(null=False, primary_key=True, editable=False)
     user_owner = models.ForeignKey(User, related_name='user_owner', null=True)
     definitions = models.ForeignKey(Definition, related_name='definitions', null=True)
-    added = models.DateTimeField(null=True, editable=False)
-    updated = models.DateTimeField(null=True, editable=False)
-
-    def save(self):
-        if not self.id:
-            self.added = timezone.now()
-        self.updated = timezone.now()
-        super(UserDefinition, self).save()
---
+++ modua/core/behaviors.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+from extras import CharNullField
+
+
+
+class Timestampable(models.Model):
+    create_date = models.DateTimeField(auto_add_now=True, null=True, editable=False)
+    modified_date = models.DateTimeField(auto_now=True, null=True, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class Authorable(models.Model):
+    author = models.ForeignKey(User, related_name='authors', null=True)
+
+    class Meta:
+        abstract = True
+
+class Editable(models.Model):
+    editor = models.ForeignKey(User, related_name='editors', null=True)
+
+    class Meta:
+        abstract = True
