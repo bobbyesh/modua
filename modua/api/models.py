4b03e2a6a1157c7073aaed186ce0b03786070ea7
bobbyesh <eshleman@pdx.edu>
2016-08-28 12:50:35 -0700

    Imported behaviors to models.

--- modua/api/models.py
+++ modua/api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from core.behaviors import Timestampable
+from core.behaviors import Timestampable, Authorable, Editable
 from core.fields import CharNullField
 
 
