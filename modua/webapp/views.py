031dc4d10c55c05cc33f0a0d228274d884c614d7
Bobby Eshleman <eshleman@pdx.edu>
2016-09-02 16:33:49 -0700

    Login_required decorator for home.

--- modua/webapp/views.py
+++ modua/webapp/views.py
@@ -1,13 +1,23 @@
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from core.utils import klassified
 from core.services import fetch_article, tokenize_text
 
 
-class HomeView(TemplateView):
+@method_decorator(login_required, name='dispatch')
+class HomeView(TemplateView, LoginRequiredMixin):
 
     template_name = 'webapp/home.html'
+    login_url = 'landing/signin'
 
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        articles = self.request.user.api_article_owner.all()
+        context['articles'] = articles
+        import pdb;pdb.set_trace()
+        return context
 
 class ArticleView(TemplateView):
 
@@ -17,18 +27,4 @@ class ArticleView(TemplateView):
         context = super(ArticleView, self).get_context_data(**kwargs)
         text = fetch_article('http://www.fox2008.cn/Article/2009/20090406000000_21827.html', 'zh')
         context['tokens'] = klassified(tokenize_text(text))
-        return context
-
-
-class CollectionView(TemplateView, LoginRequiredMixin):
-
-    template_name = 'webapp/collection.html'
-    login_url = 'landing/signin'
-    redirect_field_name = 'redirect_to'
-
-    def get_context_data(self, **kwargs):
-        context = super(CollectionView, self).get_context_data(**kwargs)
-        articles = self.request.user.api_owner_article.all()
-        context['articles'] = articles
-        import pdb;pdb.set_trace()
         return context
