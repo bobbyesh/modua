18db62f143189a5b698c6a347a066f07de324079
Bobby Eshleman <eshleman@pdx.edu>
2016-09-07 10:10:18 -0700

    Added token creation signal to models.py, see docs for why in models.py

--- modua/api/models.py
+++ modua/api/models.py
@@ -5,6 +5,21 @@ from django.core.urlresolvers import reverse
 from core.behaviors import Timestampable, Contributable, Editable, Ownable
 
 
+''' The following set of imports and the create_auth_token are placed in this models.py because
+it is guaranted that it will be imported by Django at startup, as suggest by the Django REST Framework
+docs.
+'''
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+
 class Language(Contributable, Editable, Timestampable, models.Model):
     language = models.CharField(blank=True, max_length=150)
     script = models.CharField(blank=True, max_length=300)
